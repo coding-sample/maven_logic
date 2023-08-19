82e605b1a0c93ec452079535d2b3d8a61634e838
zuruzirou <zuruzirou@gmail.com>
2023-08-19 18:40:56 +0900

    first commit.

---
+++ src/main/java/com/example/template_logic/TemplateLogicApplication.java
@@ -0,0 +1,13 @@
+package com.example.template_logic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateLogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateLogicApplication.class, args);
+	}
+
+}
